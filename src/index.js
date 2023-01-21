c7169c3d320bda505d5644fb2b2f991bc038bc2e
Daniel Moura <daniel-moura@outlook.com>
2023-01-21 15:15:36 -0300

    feat: implement check exists user account

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username)
+
+  if (!user) {
+    response.status(400).send({
+      error: 'User does not exist'
+    })
+  }
+
+  if (id) {
+    const todos = user.todos;
+    if (todos.length) {
+      const todo = todos.find(item => item.id === id)
+      if (!todo) {
+        response.status(404).send({
+          error: 'Item does not exist'
+        })
+      }
+    } else {
+      response.status(404).send({
+        error: 'Item does not exist'
+      })
+    }
+  }
+
+  next()
 }
 
 app.post('/users', (request, response) => {
